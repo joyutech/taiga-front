e8b6329ee1111baa16da2cff3b7317c9736ca566
luzhuokun <luchaokun@joyutech.com>
2019-07-05 10:06:56 +0800

    1、把node-sass改为4.12.0版本、gulp-sass改为4.0.2版本 2、把api请求的接口更换为现网接口

--- karma.app.conf.js
+++ karma.app.conf.js
@@ -1,5 +1,5 @@
 window.taigaConfig = {
-    "api": "http://localhost:8000/api/v1/",
+    "api": "http://222.44.104.36:88/api/v1/",
     "eventsUrl": null,
     "debug": true,
     "defaultLanguage": "en",
